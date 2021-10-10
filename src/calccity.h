c584111c4ee2b17ba59de156fde5a03550e32fa8
Shadow15510 <antoine.royer14@orange.fr>
2021-10-10 16:22:09 +0200

    Add some structures : calccity, camera and map

--- src/calccity.h
+++ src/calccity.h
@@ -3,10 +3,14 @@
 
 struct calccity = 
 {
-	// human's statistics = {happyness, health, education, housing, work, food, transport, grabage, graves} 
-	signed long statistics[9] = {0};
+	// human's statistics = {happyness, health, education, housing, work, food} 
+	signed long humans[6] = {0};
 
-	// population evolution = {birth, death, immigration, emmigration}
+	// population evolution = {population, birth_rate, death_rate, immigration, emigration}
+	signed long population[5] = {0};
+
+	// trade statistics = {import, export, production, commercial, industrial, annual_cost}
+	signed long trade[6] = {0};
 
 	// production = {water, power}
 	signed long production[2] = {0};
@@ -14,18 +18,40 @@ struct calccity =
 	// consumption = {water, power}
 	signed long consumption[2] = {0};
 
-	// risks and safety = {crime, safety, fire_hazard, nuclear_hazard, pollution}
-	signed long risks[5] = {0};
+	// some others statistics = {crime, safety, fire_hazard, nuclear_hazard, pollution, transport, grabage, graves}
+	signed long misc[8] = {0};
 
-	// taxes on {housing, trade, industry, export}
+	// taxes in percents on {housing, trade, industry, export}
 	int taxes[4] = {10, 10, 10, 10};
 
-	// industry statistics = {import, export, production, commercial, industrial, }
+	// fund in percents on {police, fireman, education, heathcare}
+	int funds[4] = {0};
+
+	// in-game time
+	int month = 1, year = 1900;
+	int blinker = 0;
+
+	// in-game options
+	bool disaster = true;
+	bool show_fps = false;
+	int time_speed = 30;
+
 };
 
-struct mouse = 
+
+struct camera = 
 {
+	int x, y;
+	int zoom;
 
+	int cursor_x, cursor_y;
 };
+
+
+struct map = 
+{
+	unsigned char map_data[50][50];
+};
+
 
 #endif /* _CALCCITY_H */
